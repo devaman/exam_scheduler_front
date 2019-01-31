48dd7e79e940da8384bd4c7c821806ff93648e01
Amit Chambial <chambialamit@gmail.com>
2019-01-31 15:38:28 +0530

    Uploading file created

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -1,32 +1,35 @@
-var Todo = require('../models/todo');
 var isLoggedIn = require('../middlewares/isLoggedIn');
 var authenticated_using = require('../helpers/authenticated_using')
-var fs=require('fs');
-var multer=require('multer');
-var storage=multer.diskStorage({
-    destination:function(req,file,cb){
-      var dest=__dirname+'/../../public/uploads/';
-      cb(null,dest);
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        var dest = __dirname + '/../../public/uploads/';
+        cb(null, dest);
     },
-    filename:function(req,file,cb){
-      cb(null,file.originalname);
+    filename: function (req, file, cb) {
+        let ext = file.originalname.split('.')[1];
+        cb(null, file.fieldname + '.' + ext);
     }
-  })
-  var uploading=multer({storage:storage});
+})
+var upload = multer({ storage: storage });
+var cpUpload = upload.fields([{ name: 'room_list', maxCount: 1 }, { name: 'teacher_list', maxCount: 1 }, { name: 'schedule_list', maxCount: 1 }])
 module.exports = function (app) {
 
     app.get('/admin', isLoggedIn, function (req, res) {
         auth_user = authenticated_using(req)
-      
-            //res.json(tododoc) //uncomment if developing only APIs
-            res.render('admin', {'user': auth_user})
-        })
-    app.post('/upload', isLoggedIn,uploading.array('thumbnail',3), function (req, res) {
+
+        //res.json(tododoc) //uncomment if developing only APIs
+        res.render('admin', { 'user': auth_user })
+    })
+    app.post('/upload', isLoggedIn, cpUpload, function (req, res) {
         auth_user = authenticated_using(req)
-        
-            //res.json(tododoc) //uncomment if developing only APIs
-            res.send('Uploaded successfully!')
-        })
-   
+
+        //res.json(tododoc) //uncomment if developing only APIs
+        res.send('Uploaded successfully!')
+    })
+    app.post('/start', isLoggedIn, (req, res) => {
+        res.send("finished")
+    })
+
 
 }
